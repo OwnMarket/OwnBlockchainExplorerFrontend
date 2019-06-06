8bfd064e3378864e43ac4aa7b693592687c006e2
Elvir Tabakovic <elvir.tabakovic.92@gmail.com>
2019-06-06 12:43:42 +0200

    add loader & block info data

--- Source/src/app/views/blocks/block-info/block-info.component.ts
+++ Source/src/app/views/blocks/block-info/block-info.component.ts
@@ -1,9 +1,8 @@
 import { Component, OnInit, OnDestroy, Input } from '@angular/core';
-import { ActivatedRoute, Router, ParamMap } from '@angular/router';
-
+import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Logger, untilDestroyed } from '@app/core';
-import { switchMap, finalize } from 'rxjs/operators';
 import { Observable } from 'rxjs';
+
 import { BlockStoreService } from '../block-store.service';
 
 const log = new Logger('BlockInfo');
@@ -114,29 +113,6 @@ export class BlockInfoComponent implements OnInit, OnDestroy {
       }
     ];
   }
-
-  // init() {
-  //   // TODO: Add real data
-  //   this.basicInfoConfig = [
-  //     {
-  //       label: 'Block number',
-  //       render: (item: any) => `<span style="color: #eb6723;"><strong>block.blockNumber</strong></span>`
-  //     },
-  //     {
-  //       label: 'Hash',
-  //       value: 'block.hash',
-  //       url: (item: any) => ({
-  //         route: '/transaction-info/',
-  //         params: [item.value]
-  //       })
-  //     }
-  //   ];
-
-  //   this.previousBlockConfig = [
-  //     { label: 'Previous block', value: 'previousBlock.blockNumber' },
-  //     { label: 'Hash', value: 'previousBlock.hash' }
-  //   ];
-  // }
 
   expandAdditionalInfo() {
     this.additionalInfoExpanded = !this.additionalInfoExpanded;
