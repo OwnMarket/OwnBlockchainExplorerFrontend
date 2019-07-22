5d6422b7e1c76921cec88ad33873ada9bf2b3496
Anil Mujagic <anil.mujagic@gmail.com>
2019-07-22 17:05:36 +0200

    Add a placeholder for API URL

--- Source/src/environments/environment.prod.ts
+++ Source/src/environments/environment.prod.ts
@@ -9,7 +9,7 @@ import env from './.env';
 export const environment = {
   production: true,
   version: env.npm_package_version,
-  serverUrl: 'http://localhost:9090',
+  serverUrl: '<<EXPLORER_API_URL>>',
   defaultLanguage: 'en-US',
   supportedLanguages: ['en-US']
 };
