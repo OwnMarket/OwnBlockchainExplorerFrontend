1dca7128f7570655fb855688cca0f4b913de0d71
Anil Mujagic <anil.mujagic@gmail.com>
2019-07-22 15:23:34 +0200

    Add build script to repo

---
+++ Build/build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Prevent packing of resource fork files on macOS (./._xxxxx)
+export COPYFILE_DISABLE=true
+
+set -e
+
+pushd "${0%/*}" # Go to script directory
+BUILD_DIR="$(pwd)"
+OUTPUT_DIR="$BUILD_DIR/Output"
+
+rm -rf "$OUTPUT_DIR"
+mkdir -p "$OUTPUT_DIR"
+
+echo "--- Building project..."
+cd "../Source"
+npm ci
+npm run build -- --prod
+
+echo "--- Creating package..."
+cd "dist"
+tar czf "$OUTPUT_DIR/Own.BlockchainExplorer.App.tar.gz" *
+
+popd # Go back to caller directory
+
+# Show build output
+echo "--- Build output in $OUTPUT_DIR"
+ls -lh "$OUTPUT_DIR"
