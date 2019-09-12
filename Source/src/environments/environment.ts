085259c45b4a5371487dc6054704506ef9d7c703
SKaDiZZ <akirapowered@gmail.com>
2019-09-12 22:49:29 +0200

    Reverted change on environment serverUrl

--- Source/src/environments/environment.ts
+++ Source/src/environments/environment.ts
@@ -13,7 +13,7 @@ import env from './.env';
 export const environment = {
   production: false,
   version: env.npm_package_version + '-dev',
-  serverUrl: 'https://test.explorer-api.weown.com',
+  serverUrl: '/api',
   defaultLanguage: 'en-US',
   supportedLanguages: ['en-US']
 };
