52dc60c840cefef5a36c396788cfc02280c458d3
SKaDiZZ <akirapowered@gmail.com>
2022-03-16 19:35:06 +0100

    updated toolbar component, validators page

--- Source/src/app/views/validators/validators.module.ts
+++ Source/src/app/views/validators/validators.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+import { SharedModule } from '@app/shared';
+import { DropdownModule, IconsModule } from '@app/shared/components';
 
 import { ValidatorsRoutingModule } from './validators-routing.module';
 import { ValidatorsComponent } from './validators.component';
-import { SharedModule } from '@app/shared';
 import { ValidatorsMapComponent } from './validators-map/validators-map.component';
 import { ValidatorTimeFilterComponent } from './validator-time-filter/validator-time-filter.component';
 
 @NgModule({
   declarations: [ValidatorsComponent, ValidatorsMapComponent, ValidatorTimeFilterComponent],
-  imports: [CommonModule, SharedModule, LeafletModule.forRoot(), ValidatorsRoutingModule]
+  imports: [CommonModule, SharedModule, LeafletModule.forRoot(), DropdownModule, IconsModule, ValidatorsRoutingModule]
 })
 export class ValidatorsModule {}
