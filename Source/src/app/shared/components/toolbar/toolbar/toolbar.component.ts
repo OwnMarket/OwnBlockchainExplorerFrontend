92e6f3434da5a52b5024682352adc4b48b97b805
SKaDiZZ <akirapowered@gmail.com>
2022-03-22 16:49:39 +0100

    added dark mode support

--- Source/src/app/shared/components/toolbar/toolbar/toolbar.component.ts
+++ Source/src/app/shared/components/toolbar/toolbar/toolbar.component.ts
@@ -5,9 +5,10 @@ import { DropdownComponent } from '../../dropdown/dropdown/dropdown.component';
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
-  styleUrls: ['./toolbar.component.scss']
+  styleUrls: ['./toolbar.component.scss'],
 })
 export class ToolbarComponent implements OnInit {
+  theme: 'light' | 'dark' = 'light';
   selectedLanguage: string;
   showUnloadWallet: boolean = false;
 
@@ -16,7 +17,17 @@ export class ToolbarComponent implements OnInit {
     if (currentLang) this.selectedLanguage = currentLang;
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    if (
+      localStorage.getItem('theme') === 'dark' ||
+      (!localStorage.getItem('theme') && window.matchMedia('(prefers-color-scheme: dark)').matches)
+    ) {
+      this.theme = 'dark';
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }
 
   selectLanguage(language: string) {
     localStorage.setItem('selectedLanguage', language);
@@ -31,5 +42,15 @@ export class ToolbarComponent implements OnInit {
   showUnloadWalletModal(dropdown: DropdownComponent): void {
     this.showUnloadWallet = true;
     dropdown.toggle();
+  }
+
+  setTheme(theme: 'light' | 'dark'): void {
+    this.theme = theme;
+    localStorage.setItem('theme', theme);
+    if (theme === 'dark') {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
   }
 }
