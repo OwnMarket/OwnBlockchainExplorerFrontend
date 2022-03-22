d97b94d61968f9ae4b16d3e630eb9edcfd1f2482
SKaDiZZ <akirapowered@gmail.com>
2022-03-22 07:52:20 +0100

    translated transaction info page

--- Source/src/app/views/transaction-info/transaction-info.module.ts
+++ Source/src/app/views/transaction-info/transaction-info.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { TransactionInfoRoutingModule } from './transaction-info-routing.module';
-import { SharedModule } from '@app/shared';
 import { TranslateModule } from '@ngx-translate/core';
+import { SharedModule } from '@app/shared';
+import { TransactionInfoRoutingModule } from './transaction-info-routing.module';
+
 import { TransactionInfoComponent } from './transaction-info.component';
 
 @NgModule({
   declarations: [TransactionInfoComponent],
-  imports: [CommonModule, TranslateModule, SharedModule, TransactionInfoRoutingModule],
+  imports: [CommonModule, TranslateModule.forChild(), SharedModule, TransactionInfoRoutingModule],
   providers: [],
-  exports: [TransactionInfoComponent]
+  exports: [TransactionInfoComponent],
 })
 export class TransactionInfoModule {}
